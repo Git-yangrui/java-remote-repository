0fe64db0572a7370db698a38159d62bcc6bccbd7
yangrui(admin) <64537132@qq.com>
2017-04-09 12:31:39 +0800

    comit

--- nio/src/main/java/com/citi/nio/ByteBufferTest.java
+++ nio/src/main/java/com/citi/nio/ByteBufferTest.java
@@ -1,5 +1,21 @@
 package com.citi.nio;
 
+import java.nio.ByteBuffer;
+
+import org.junit.Test;
+
 public class ByteBufferTest {
- 
+  @Test
+  public void test_bytebuffer(){
+	  ByteBuffer byteBuffer=ByteBuffer.allocate(10);
+	  byteBuffer.put((byte)'H').put((byte)'A').put((byte)'B').put((byte)'B').put((byte)'B');
+	  System.out.println(byteBuffer);
+	  byteBuffer.flip();
+	  System.out.println(byteBuffer);
+	  byte mybyteArray[] =new byte[10];
+	  for(int i=0;byteBuffer.hasRemaining();i++){
+		  mybyteArray[i]=byteBuffer.get();
+		  System.out.println(byteBuffer);
+	  }
+  }
 }
