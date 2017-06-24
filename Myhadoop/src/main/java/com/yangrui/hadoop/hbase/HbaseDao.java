e0a99861594ca01ad7fa0219bea2c09ffc99f018
yangrui(admin) <64537132@qq.com>
2017-06-24 15:16:23 +0800

    1213

--- Myhadoop/src/main/java/com/yangrui/hadoop/hbase/HbaseDao.java
+++ Myhadoop/src/main/java/com/yangrui/hadoop/hbase/HbaseDao.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.MasterNotRunningException;
 import org.apache.hadoop.hbase.TableName;
@@ -18,6 +19,10 @@ public class HbaseDao {
 		HBaseAdmin admin = new HBaseAdmin(configuration);
 		TableName tableName = TableName.valueOf("mygirls");
 		HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
+		HColumnDescriptor base_info=new HColumnDescriptor("base_info");
+		base_info.setMaxVersions(3);
+		
+		hTableDescriptor.addFamily(base_info);
 		admin.createTable(hTableDescriptor);
 	}
 }
