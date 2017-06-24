4d6060967878f7fdc4301984beaef591391b88b5
yangrui(admin) <64537132@qq.com>
2017-06-24 12:35:02 +0800

    123

---
+++ Myhadoop/src/main/java/com/yangrui/hadoop/hbase/HbaseDao.java
@@ -0,0 +1,23 @@
+package com.yangrui.hadoop.hbase;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.MasterNotRunningException;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.ZooKeeperConnectionException;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+
+public class HbaseDao {
+	public static void main(String[] args) throws MasterNotRunningException, ZooKeeperConnectionException, IOException {
+		Configuration configuration = HBaseConfiguration.create();
+		configuration.set("hbase.zookeeper.quorum", "yangziyu01:2181,yangziyu02:2181,yangziyu03:2181");
+		@SuppressWarnings("deprecation")
+		HBaseAdmin admin = new HBaseAdmin(configuration);
+		TableName tableName = TableName.valueOf("mygirls");
+		HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
+		admin.createTable(hTableDescriptor);
+	}
+}
