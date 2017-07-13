85e78ec7ad4f2515e50280120124cf96ac3e0bf6
yangrui <645374132@qq.com>
2017-07-13 23:59:21 +0800

    commit

---
+++ activemq/src/main/java/com/yangrui/activemq/activemq/App.java
@@ -0,0 +1,13 @@
+package com.yangrui.activemq.activemq;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
